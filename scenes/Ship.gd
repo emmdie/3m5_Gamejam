b30effb1d7a8654c29b9296e34d35c6a62a59f7a
PhilippRiegelmann <philipp.riegelmann@outlook.de>
2022-10-29 16:24:16 +0200

    changed ship image

--- scenes/Ship.gd
+++ scenes/Ship.gd
@@ -7,9 +7,8 @@ var velocity = Vector2.ZERO
 
 func _ready():
 	screen_size = get_viewport().size
-	
 
-func _process(delta):
+func slowdown(velocity):
 	if abs(velocity.y) >=0.5:
 		velocity.y *= 0.95
 	else: 
@@ -18,24 +17,44 @@ func _process(delta):
 		velocity.x *= 0.95
 	else: 
 		velocity.x = 0
+	return velocity
+	
+
+func _process(delta):
+	velocity = slowdown(velocity)
+	var input_bool = false
 		
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1*acceleration
+		input_bool = true
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1*acceleration
+		input_bool = true
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1*acceleration
+		input_bool = true
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1*acceleration
+		input_bool = true
 
 	if velocity.length() > 0:
 		if velocity.length() > max_speed:
 			velocity = velocity.normalized() * max_speed
-		$Sprite.set_rotation(velocity.angle()-PI/2)
-		$Particles2D.set_rotation(-velocity.angle())
+		$AnimatedSprite.play()
+		$Particles2D.set_emitting(true)
+		if input_bool:
+			if abs(velocity.x) < 10 and abs(velocity.y) !=0 :
+				$AnimatedSprite.animation = "Non_Horizontal"
+			else:
+				$AnimatedSprite.animation = "Horizontal"
+			
+			$AnimatedSprite.set_rotation(velocity.angle()-PI)
+			$Particles2D.set_rotation(-velocity.angle())
+			$AnimatedSprite.flip_v = velocity.x < 0
 		position += velocity * delta
 		position.x = clamp(position.x, 0, screen_size.x)
 		position.y = clamp(position.y, 0, screen_size.y)
-		$Particles2D.set_emitting(true)
+		
 	else:
 		$Particles2D.set_emitting(false)
+		$AnimatedSprite.stop()
