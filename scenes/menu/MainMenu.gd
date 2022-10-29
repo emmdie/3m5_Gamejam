b9c7eba6f275c2c1f3062103a4491202592c16dd
emmdie <emmanuel.diehl@ifsr.de>
2022-10-29 15:04:59 +0200

    finish fixing merge conflict

--- scenes/menu/MainMenu.gd
+++ scenes/menu/MainMenu.gd
@@ -1,15 +1,12 @@
 extends MarginContainer
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -17,3 +14,6 @@ func _ready():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://scenes/Level.tscn")
