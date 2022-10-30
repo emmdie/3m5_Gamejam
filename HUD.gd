dfdc4902fe86e10cf19506972d2d481e2109bfed
PhilippRiegelmann <philipp.riegelmann@outlook.de>
2022-10-30 18:29:34 +0100

    Create HUD

--- HUD.gd
+++ HUD.gd
@@ -11,3 +11,4 @@ func _process(delta):
 	$Amount_Shoes.text = str(GlobalVariables.items["shoe"])
 	$Amount_Flowerpots.text = str(GlobalVariables.items["flowerpot"])
 	$Amount_Ribonukleoproteins.text = str(GlobalVariables.items["ribonukleoprotein"])
+	$Amount_Health.text = str(GlobalVariables.health)
